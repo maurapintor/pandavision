fix: efficient PGD computation, default params

app/adv/evaluation_manager.py
@@ -157,7 +157,7 @@ class EvaluationManager:
         batch_size = self._validation_loader.batch_size
         self._batch_is_cached = [False for _ in range(len(self._validation_loader))]
         self.cached_is_adv = torch.full(size=(self._num_samples,), fill_value=False)
-        self.cached_min_distance = torch.full(size=(self._num_samples,), fill_value=np.inf)
+        self.cached_min_distance = torch.full(size=(self._num_samples,), fill_value=np.inf, dtype=torch.float64)
 
         for eps in self._perturbation_values:
             acc = []
@@ -183,24 +183,23 @@ class EvaluationManager:
                 else:
                     is_adv_batch = self.cached_is_adv[batch_idx * batch_size:
                                                       min((batch_idx + 1) * batch_size, self._num_samples)]
-                    not_yet_adv = torch.where(torch.logical_not(is_adv_batch), True, False)
-                    is_adv, adv_points = self.attack.run(samples[not_yet_adv, ...], labels[not_yet_adv, ...],
-                                                         self._attack_cls, self._attack_params, eps)
-                    is_adv = torch.from_numpy(is_adv)
-                    adv_points = torch.from_numpy(adv_points)
-                    now_adv = not_yet_adv[is_adv]
-                    if len(now_adv) > 0:
+                    distances_batch = self.cached_min_distance[batch_idx * batch_size:
+                                                               min((batch_idx + 1) * batch_size, self._num_samples)]
+                    not_yet_adv = torch.nonzero(torch.logical_not(is_adv_batch))
+                    if len(not_yet_adv) > 0:
+                        is_adv, adv_points = self.attack.run(samples[not_yet_adv, ...], labels[not_yet_adv, ...],
+                                                             self._attack_cls, self._attack_params, eps)
+                        is_adv = torch.from_numpy(is_adv)
+                        is_adv_batch[not_yet_adv[is_adv]] = is_adv
+                        adv_points = torch.from_numpy(adv_points)
                         self.cached_is_adv[batch_idx * batch_size:
-                                           min((batch_idx + 1) * batch_size, self._num_samples)] = now_adv
-                    distances = (adv_points - samples).view(adv_points.shape[0], -1).norm(dim=1,
-                                                                                          p=self.attack.attack_norm(
-                                                                                              self._attack_cls))
-                    self.cached_min_distance[now_adv] = distances[is_adv]
-                    self.cached_min_distance[torch.logical_not(self.cached_is_adv)] = np.inf
-
-                print("min dist", self.cached_min_distance)
-                print("not adv: ", torch.logical_not(self.cached_is_adv).tolist())
-                print("dist constr:", (torch.logical_and(self.cached_is_adv, self.cached_min_distance > eps).tolist()))
+                                           min((batch_idx + 1) * batch_size, self._num_samples)] = is_adv_batch
+                        distances = (adv_points - samples).view(adv_points.shape[0], -1).norm(dim=1,
+                                                                                              p=self.attack.attack_norm(
+                                                                                                  self._attack_cls))
+                        distances_batch[is_adv_batch] = distances
+                        self.cached_min_distance[batch_idx * batch_size:
+                                                 min((batch_idx + 1) * batch_size, self._num_samples)] = distances_batch
                 perf = torch.logical_or(torch.logical_not(self.cached_is_adv),
                                         torch.logical_and(self.cached_is_adv, self.cached_min_distance > eps)) \
                     .type(torch.FloatTensor).mean()